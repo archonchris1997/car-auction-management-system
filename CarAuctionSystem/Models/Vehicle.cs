9d054acb77eb07823848218e0db0401d5e05a756
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-03-23 01:23:51 +0200

    Vehicle class created

---
+++ CarAuctionSystem/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+using CarAuctionSystem.Utils;
+
+namespace CarAuctionSystem.Models;
+
+public abstract class Vehicle
+{
+    public Guid Id { get; }
+    public VehicleType Type { get; }
+    public string Manufacturer { get; }
+    public string Model { get; }
+    public int Year { get; }
+    public double StartingBid { get; }
+ 
+    protected Vehicle(VehicleType type, string manufacturer, string model, int year, double startingBid)
+    {
+        Id = Guid.NewGuid();
+        Type = type;
+        Manufacturer = manufacturer;
+        Model = model;
+        Year = year;
+        StartingBid = startingBid;
+    }
+}
---
+++ CarAuctionSystem/Utils/VehicleType.cs
@@ -0,0 +1,9 @@
+namespace CarAuctionSystem.Utils;
+
+public enum VehicleType
+{
+    Hatchback = 1,
+    Sedan = 2,
+    Suv = 3,
+    Truck = 4
+}
