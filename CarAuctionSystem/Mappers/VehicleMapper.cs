9dcf480159024ebc3d87a0b895dcf7da619526a8
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-03-24 01:23:26 +0200

    Vehicle Mapper created

---
+++ CarAuctionSystem/Mappers/VehicleMapper.cs
@@ -0,0 +1,47 @@
+using CarAuctionSystem.Dtos;
+using CarAuctionSystem.Models;
+
+namespace CarAuctionSystem.Mappers;
+
+public class VehicleMapper
+{
+    public static VehicleDto ConvertToDto(Vehicle vehicle)
+    {
+        var dto = new VehicleDto
+        {
+            Id = vehicle.Id,
+            Type = vehicle.Type,
+            Manufacturer = vehicle.Manufacturer,
+            Model = vehicle.Model,
+            Year = vehicle.Year,
+            StartingBid = vehicle.StartingBid
+        };
+ 
+        if (vehicle is Sedan)
+        {
+            var sedan = (Sedan)vehicle;
+            dto.NumberOfDoors = sedan.NumberOfDoors;
+        }
+        else if (vehicle is Hatchback)
+        {
+            var hatchback = (Hatchback)vehicle;
+            dto.NumberOfDoors = hatchback.NumberOfDoors;
+        }
+        else if (vehicle is Suv)
+        {
+            var suv = (Suv)vehicle;
+            dto.NumberOfSeats = suv.NumberOfSeats;
+        }
+        else if (vehicle is Truck)
+        {
+            var truck = (Truck)vehicle;
+            dto.LoadCapacity = truck.LoadCapacity;
+        }
+        else
+        {
+            throw new InvalidOperationException($"Unsupported vehicle type: {vehicle.GetType().Name}");
+        }
+ 
+        return dto;
+    }
+}
--- CarAuctionSystem/Services/VehicleService.cs
+++ CarAuctionSystem/Services/VehicleService.cs
@@ -7,7 +7,7 @@ using CarAuctionSystem.Validation;
 
 namespace CarAuctionSystem.Services;
 
-public class VehicleService
+public class VehicleService:IVehicleService
 {
     private readonly IVehicleRepository _repository;
     private readonly ICreateVehicleValidator _validator;
@@ -23,10 +23,63 @@ public class VehicleService
         _factory = factory;
     }
 
-    public OperationResult<VehicleDto> AddVehicle(Vehicle vehicle)
+
+    public OperationResult<VehicleDto> AddVehicle(CreateVehicleRequest request)
     {
-        
-        
+        var errors = _validator.Validate(request);
+        if (errors.Count > 0)
+        {
+            return new OperationResult<VehicleDto>
+            {
+                Success = false,
+                Message = "Validation failed",
+                Errors = errors,
+                ErrorType = ErrorType.Validation,
+                Data = null
+            };
+        }
+ 
+        var vehicle = _factory.Create(request);
+ 
+        if (_repository.GetById(vehicle.Id) != null)
+        {
+            return new OperationResult<VehicleDto>
+            {
+                Success = false,
+                Message = "Vehicle already exists",
+                Errors = new List<string> { "Id must be unique" },
+                ErrorType = ErrorType.Conflict,
+                Data = null
+            };
+        }
+ 
+        _repository.Insert(vehicle);
+ 
+        return new OperationResult<VehicleDto>
+        {
+            Success = true,
+            Message = "Vehicle added",
+            Data = VehicleMapper.ConvertToDto(vehicle)
+        };
     }
-    
+
+    public OperationResult<List<VehicleDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public OperationResult<List<VehicleDto>> GetByManufacturer(string manufacturer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OperationResult<List<VehicleDto>> GetByModel(string model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OperationResult<List<VehicleDto>> GetByYear(int year)
+    {
+        throw new NotImplementedException();
+    }
 }
