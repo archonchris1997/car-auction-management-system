d87a3e687bc45d8055609f010bbc7c63e4360b23
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-03-23 20:12:14 +0200

    Inicio do VehicleService

--- CarAuctionSystem/Services/VehicleService.cs
+++ CarAuctionSystem/Services/VehicleService.cs
@@ -9,6 +9,15 @@ public class VehicleService
     private readonly IVehicleRepository _repository;
     private readonly ICreateVehicleValidator _validator;
     private readonly IVehicleFactory _factory;
-
+    
+    public VehicleService(
+        IVehicleRepository repository,
+        ICreateVehicleValidator validator,
+        IVehicleFactory factory)
+    {
+        _repository = repository;
+        _validator = validator;
+        _factory = factory;
+    }
     
 }
