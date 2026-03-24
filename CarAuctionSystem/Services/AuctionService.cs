8a426ea2a2f963e36ce2db4d022a2e3933326f55
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-03-24 19:34:37 +0200

    AuctionService - edited

--- CarAuctionSystem/Services/AuctionService.cs
+++ CarAuctionSystem/Services/AuctionService.cs
@@ -1,4 +1,6 @@
 using CarAuctionSystem.Dtos;
+using CarAuctionSystem.Mappers;
+using CarAuctionSystem.Models;
 using CarAuctionSystem.Repository;
 using CarAuctionSystem.Utils;
 
@@ -21,7 +23,7 @@ public class AuctionService:IAuctionService
         var vehicle = _vehicleRepository.GetById(vehicleId);
         if (vehicle == null)
         {
-            return new OperationResult<AuctionDto>()
+            return new OperationResult<AuctionDto>
             {
                 Success = false,
                 Message = "Vehicle does not exist",
@@ -30,9 +32,9 @@ public class AuctionService:IAuctionService
                 Data = null
             };
         }
-        
-        var auction = _auctionRepository.GetByVehicleId(vehicleId);
-        if (auction != null)
+ 
+        var existingAuction = _auctionRepository.GetByVehicleId(vehicleId);
+        if (existingAuction != null)
         {
             return new OperationResult<AuctionDto>
             {
@@ -42,29 +44,118 @@ public class AuctionService:IAuctionService
                 ErrorType = ErrorType.Conflict,
                 Data = null
             };
-                
         }
-        
-        auction.Active =  true;
+ 
+        var auction = new Auction(vehicle);
+        auction.Active = true;
         _auctionRepository.Insert(auction);
-        
+ 
         return new OperationResult<AuctionDto>
         {
             Success = true,
             Message = "Auction started",
             Data = AuctionMapper.ConvertToDto(auction)
         };
-        
-        
     }
-
+    
     public OperationResult<AuctionDto> CloseAuction(Guid vehicleId)
     {
-        throw new NotImplementedException();
+        var vehicle = _vehicleRepository.GetById(vehicleId);
+        if (vehicle == null)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Vehicle does not exist",
+                Errors = new List<string> { "Vehicle not found" },
+                ErrorType = ErrorType.NotFound,
+                Data = null
+            };
+        }
+ 
+        var auction = _auctionRepository.GetByVehicleId(vehicleId);
+        if (auction == null)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Auction does not exist",
+                Errors = new List<string> { "Auction not found" },
+                ErrorType = ErrorType.NotFound,
+                Data = null
+            };
+        }
+ 
+        if (auction.Active == false)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Auction is already closed",
+                Errors = new List<string> { "Auction is already closed" },
+                ErrorType = ErrorType.Conflict,
+                Data = null
+            };
+        }
+ 
+        auction.Active = false;
+        _auctionRepository.Update(auction);
+ 
+        return new OperationResult<AuctionDto>
+        {
+            Success = true,
+            Message = "Auction closed",
+            Data = AuctionMapper.ConvertToDto(auction)
+        };
     }
 
     public OperationResult<AuctionDto> PlaceBid(Guid vehicleId, int bid)
     {
-        throw new NotImplementedException();
+        var auction = _auctionRepository.GetByVehicleId(vehicleId);
+        if (auction == null)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Auction does not exist",
+                Errors = new List<string> { "Auction not found" },
+                ErrorType = ErrorType.NotFound,
+                Data = null
+            };
+        }
+ 
+        if (auction.Active == false)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Auction is not active",
+                Errors = new List<string> { "Cannot place a bid on a closed auction" },
+                ErrorType = ErrorType.Conflict,
+                Data = null
+            };
+        }
+ 
+        if (bid <= auction.CurrentBid)
+        {
+            return new OperationResult<AuctionDto>
+            {
+                Success = false,
+                Message = "Bid is too low",
+                Errors = new List<string> { "Bid must be higher than current bid" },
+                ErrorType = ErrorType.Validation,
+                Data = null
+            };
+        }
+ 
+        auction.CurrentBid = bid;
+        _auctionRepository.Update(auction);
+ 
+        return new OperationResult<AuctionDto>
+        {
+            Success = true,
+            Message = "Bid placed successfully",
+            Data = AuctionMapper.ConvertToDto(auction)
+        };
     }
 }
