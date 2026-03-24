437dbc5d63a45c864ddefd3e59f2b27dbf99e1e8
archonchris1997 <94084676+archonchris1997@users.noreply.github.com>
2026-03-24 18:36:26 +0200

    AuctionRepository - edited

--- CarAuctionSystem/Repository/AuctionRepository.cs
+++ CarAuctionSystem/Repository/AuctionRepository.cs
@@ -1,6 +1,36 @@
+using CarAuctionSystem.Models;
+
 namespace CarAuctionSystem.Repository;
 
-public class AuctionRepository
+public class AuctionRepository:IAuctionRepository
 {
     
+    
+    private readonly List<Auction> _auctions = new();
+ 
+    public Auction? GetByVehicleId(Guid vehicleId)
+    {
+        return _auctions.FirstOrDefault(a => a.Vehicle.Id == vehicleId);
+    }
+ 
+    public List<Auction> GetAll()
+    {
+        return _auctions;
+    }
+ 
+    public void Insert(Auction auction)
+    {
+        _auctions.Add(auction);
+    }
+ 
+    public void Update(Auction auction)
+    {
+        var existing = _auctions.FirstOrDefault(a => a.Id == auction.Id);
+        
+        if (existing != null)
+        {
+            existing.Active = auction.Active;
+            existing.CurrentBid = auction.CurrentBid;
+        }
+    }
 }
